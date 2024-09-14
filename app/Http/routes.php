f5dda7cf1c922804db055fdbee75b085b7ef7bb2
Beiri Alife <beiri.al@outlook.com>
2024-09-14 14:34:57 -0300

    Ep 04 - Broadcasting With Redis

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Events\UserSignedUp;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,14 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $data = [
-        'event' => 'UserSignedUp',
-        'data' => [
-            'username' => 'JohnDoe'
-        ],
-    ];
-
-    Redis::publish('test-channel', json_encode($data));
+    event(new UserSignedUp(Request::query('name')));
 
     return view('welcome');
 });
